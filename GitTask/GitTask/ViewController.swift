9464a0fa7a0f102559968a0d68f8eab9a4323030
BassantELmekkawy <36138408+BassantELmekkawy@users.noreply.github.com>
2023-08-11 10:58:59 +0300

    Initial Commit

---
+++ GitTask/GitTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTask
+//
+//  Created by Bassant on 11/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
